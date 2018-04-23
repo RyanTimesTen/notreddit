efc3b157a08230c9e3cb9bed03cbbfb00905234f
Ryan <ryangilbert7926@gmail.com>
2018-04-22 22:42:10 -0400

    Minor refactoring + some code documentation

--- src/api.js
+++ src/api.js
@@ -3,29 +3,30 @@ import qs from 'qs';
 
 const REDDIT_API_URL = 'https://oauth.reddit.com';
 
-const get = (url, params, token) => {
-  const options = {
+/**
+ * Generic function to hit a Reddit API endpoint with options and auth
+ *
+ * @param {string} endpoint The Reddit API endpoint to hit
+ * @param {object} params The options to pass to the Reddit API endpoint
+ * @param {string} token Reddit OAuth token
+ */
+const get = (url, params, token) =>
+  fetch(params ? `${url}?${qs.stringify(params)}` : url, {
     method: 'GET',
     headers: {
       Authorization: `bearer ${token}`,
     },
-  };
+  }).then(res => res.json());
 
-  return fetch(params ? `${url}?${qs.stringify(params)}` : url, options)
-    .then(res => res.json());
-};
+export const getPosts = (type, token, params = null) =>
+  get(`${REDDIT_API_URL}/${type}`, params, token)
+    .then(res => res.data.children.map(post => post.data))
+    .catch(err => console.log(err)); // TODO: Do some real logging
 
-export const getPosts = (type, token, params = null) => {
-  const url = `${REDDIT_API_URL}/${type}`;
-  return get(url, params, token);
-};
+export const getComments = (post, token, params = null) =>
+  get(`${REDDIT_API_URL}/comments/${post}`, params, token);
 
-export const getComments = (post, token, params = null) => {
-  const url = `${REDDIT_API_URL}/comments/${post}`;
-  return get(url, params, token);
-};
-
-export const getUser = (username, token) => {
-  const url = `${REDDIT_API_URL}/user/${username}/about`;
-  return get(url, null, token);
-};
+export const getUser = (username, token) =>
+  get(`${REDDIT_API_URL}/user/${username}/about`, null, token).then(
+    res => res.data,
+  );
