1d7a38c902f1f80c5c00b949ed277178ea282a80
Ryan Gilbert <ryan.gilbert@formidable.com>
2019-12-25 22:13:38 -0500

    Improve api

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@ const fetch = require('node-fetch');
 const qs = require('qs');
 
 class Api {
-  constructor({ baseUrl } = {}) {
-    this.baseUrl = baseUrl || 'https://oauth.reddit.com';
+  constructor({ baseUrl = 'https://oauth.reddit.com' } = {}) {
+    this.baseUrl = baseUrl;
     this.token = null;
   }
 
@@ -20,14 +20,16 @@ class Api {
     };
   }
 
-  async get(endpoint, params) {
+  async get(endpoint, params = {}) {
     const fetchOptions = this.getFetchOptions();
+    let url = this.baseUrl + endpoint;
+
+    if (Object.keys(params).length > 0) {
+      url += '?' + qs.stringify(params);
+    }
 
     try {
-      const response = await fetch(
-        params ? `${this.baseUrl}${endpoint}?${qs.stringify(params)}` : url,
-        fetchOptions
-      );
+      const response = await fetch(url, fetchOptions);
 
       if (response.status === 401) {
         throw new Error(response.statusText);
@@ -40,7 +42,7 @@ class Api {
     }
   }
 
-  async getPosts(listing, params = null) {
+  async getPosts(listing, params = {}) {
     try {
       const response = await this.get(`/${listing}`, params);
       return response.data.children.map(post => post.data);
@@ -50,7 +52,7 @@ class Api {
     }
   }
 
-  async getComments(post, params = null) {
+  async getComments(post, params = {}) {
     try {
       const response = await this.get(`/comments/${post}`, params);
       return response[1].data.children
@@ -64,7 +66,7 @@ class Api {
 
   async getUser(username) {
     try {
-      const response = await this.get(`/user/${username}/about`, null);
+      const response = await this.get(`/user/${username}/about`);
       return response.data;
     } catch (error) {
       console.log(`Failed to fetch user ${username}: ${error.message}`);
