aea0fd85b3d3a0e9bc581b7045c752fb20949e45
Ryan Gilbert <ryan.gilbert@formidable.com>
2019-12-28 01:04:03 -0500

    the great restructure part 2

---
+++ src/lambda/graphql.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require("apollo-server-express");
+const { createServer } = require("http");
+const express = require("express");
+
+const schema = require("./helpers/schema");
+const { createApi } = require("./helpers/api");
+
+const DEV = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 3000;
+
+const api = createApi();
+
+const server = new ApolloServer({
+  typeDefs: schema.typeDefs,
+  resolvers: schema.resolvers,
+  context: ({ req }) => {
+    const token = (req.headers.authorization || "").split(" ")[1];
+    api.setAuth(token);
+    return { api };
+  },
+  cors: true,
+  tracing: DEV,
+  introspection: DEV,
+  playground: DEV
+    ? {
+        endpoint: "/graphql"
+      }
+    : false
+});
+
+exports.handler = server.createHandler();
+
+// const app = express();
+
+// server.applyMiddleware({ app });
+
+// const http = createServer(app);
+// http.listen(PORT);
