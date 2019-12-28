642a15beb3f87f6cf17a95aac67917fcf69f0f8c
Ryan Gilbert <ryan.gilbert@formidable.com>
2019-12-28 01:26:18 -0500

    pls i beg

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,11 +1,9 @@
-const { ApolloServer } = require("apollo-server-express");
-const { createServer } = require("http");
-const express = require("express");
+const { ApolloServer } = require('apollo-server-lambda');
 
-const schema = require("./helpers/schema");
-const { createApi } = require("./helpers/api");
+const schema = require('./helpers/schema');
+const { createApi } = require('./helpers/api');
 
-const DEV = process.env.NODE_ENV !== "production";
+const DEV = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 3000;
 
 const api = createApi();
@@ -14,7 +12,7 @@ const server = new ApolloServer({
   typeDefs: schema.typeDefs,
   resolvers: schema.resolvers,
   context: ({ req }) => {
-    const token = (req.headers.authorization || "").split(" ")[1];
+    const token = (req.headers.authorization || '').split(' ')[1];
     api.setAuth(token);
     return { api };
   },
@@ -23,16 +21,9 @@ const server = new ApolloServer({
   introspection: DEV,
   playground: DEV
     ? {
-        endpoint: "/graphql"
+        endpoint: '/graphql',
       }
-    : false
+    : false,
 });
 
 exports.handler = server.createHandler();
-
-// const app = express();
-
-// server.applyMiddleware({ app });
-
-// const http = createServer(app);
-// http.listen(PORT);
